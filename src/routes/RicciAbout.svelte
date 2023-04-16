75bd4ee4a6ea29eb49044ce2b53e2923505f1f59
0x6861796169 <sayak@medici.haus>
2023-04-16 03:23:21 -0400

    Ricci about section now follows ucc colors

--- src/routes/RicciAbout.svelte
+++ src/routes/RicciAbout.svelte
@@ -1,26 +1,46 @@
-<div class="container" id="aboutRicci">
+<script>
+    import { normalHue } from "../ucc-script/retrieve";
+    import { onMount } from "svelte";
+    let time = new Date();
+
+    $: [r, g, b] = normalHue(time);
+
+    onMount(() => {
+        const interval = setInterval(() => {
+            time = new Date();
+        }, 1000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    });
+</script>
+
+<div class="container" id="aboutRicci" style="background-color: rgb({`${r},${g},${b}`})">
     <div class="inner-container">
-    <p>
-        Ricci Albenda (b. 1966, Brooklyn) lives and works in New York.
-        In 2020, Albenda had a solo exhibition at Parrasch Heinjen, Los Angeles, and in 2018, at
-        'T'space, Rhineback, New York. Other past solo exhibitions include the Museum of Modern Art,
-        New York, and The Rachofsky House, Dallas. His work has been included in numerous group
-        exhibitions at venues which include, The Guggenheim Museum, New York , Tel Aviv Museum of Art, Tel Aviv,
-        Institute of Contemporary Art, Boston, Brooklyn Museum of Art, Brooklyn, Castello Di Rivoli, Turin,
-        Museum of Modern Art, New York, MoMA PS1, Queens, Centre Georges Pompidou, Paris, and the New Museum, New York.
-        Ricci Albenda's work is held in the permanent collections of the Musuem of Modern Art, New York, the Dallas Museum of Art, Dallas,
-        Albright-Knox Art Gallery, Buffalo, Castello di Rivoli, Turin, The Solomon R. Guggenheim Museum, New York, and the 
-        Hessel Museum of Art, Annadale-on-Hudson, New York.
-    </p>
+        <p>
+            Ricci Albenda (b. 1966, Brooklyn) lives and works in New York. In
+            2020, Albenda had a solo exhibition at Parrasch Heinjen, Los
+            Angeles, and in 2018, at 'T'space, Rhineback, New York. Other past
+            solo exhibitions include the Museum of Modern Art, New York, and The
+            Rachofsky House, Dallas. His work has been included in numerous
+            group exhibitions at venues which include, The Guggenheim Museum,
+            New York , Tel Aviv Museum of Art, Tel Aviv, Institute of
+            Contemporary Art, Boston, Brooklyn Museum of Art, Brooklyn, Castello
+            Di Rivoli, Turin, Museum of Modern Art, New York, MoMA PS1, Queens,
+            Centre Georges Pompidou, Paris, and the New Museum, New York. Ricci
+            Albenda's work is held in the permanent collections of the Musuem of
+            Modern Art, New York, the Dallas Museum of Art, Dallas,
+            Albright-Knox Art Gallery, Buffalo, Castello di Rivoli, Turin, The
+            Solomon R. Guggenheim Museum, New York, and the Hessel Museum of
+            Art, Annadale-on-Hudson, New York.
+        </p>
     </div>
 </div>
 
 <style>
-
     .container {
         display: flex;
-        background-color: #A27AEF;
-        
         justify-content: center;
         align-items: center;
         height: 586px;
@@ -28,11 +48,10 @@
 
     .inner-container {
         /* font-family: 'Raleway', sans-serif; */
-        font-family: 'Montserrat', sans-serif;
+        font-family: "Montserrat", sans-serif;
         font-weight: 200;
         font-size: 28px;
         width: 1310px;
         display: flex;
-
     }
 </style>
