67611955caee3a1669b7a8e400ad444a3418d83a
0x6861796169 <sayak@medici.haus>
2023-04-24 07:22:22 -0400

    saturated color clock updated

--- src/routes/colorclocksaturated/+page.svelte
+++ src/routes/colorclocksaturated/+page.svelte
@@ -3,8 +3,16 @@
     import { onMount } from "svelte";
 
     let time = new Date();
+    let expandVisible = true;
+    /**
+     * @type {any}
+     */
+    let timeout;
+    const expandButtonTimeout = 3000;
 
     $: [r, g, b] = saturatedHue(time);
+
+
 
     onMount(() => {
         const interval = setInterval(() => {
@@ -15,11 +23,112 @@
             clearInterval(interval);
         };
     });
+
+    let isFullscreen = false;
+
+    /**
+     * @param {{ key: any; }} event
+     */
+    function keyEventHandler(event) {
+        if (event.key === "Escape") {
+            if (isFullscreen) exitFullScreen();
+
+            console.log("Esc press detected");
+        } else if (event.key === "f") {
+            isFullscreen ? exitFullScreen() : enterFullScreen();
+            isFullscreen = !isFullscreen;
+
+            console.debug("F press detected");
+        }
+    }
+
+    function enterFullScreen() {
+        if (!document.fullscreenElement) {
+            if (document.documentElement.requestFullscreen) {
+                document.documentElement.requestFullscreen();
+            } else if (document.documentElement.mozRequestFullScreen) {
+                // Firefox
+                document.documentElement.mozRequestFullScreen();
+            } else if (document.documentElement.webkitRequestFullscreen) {
+                // Chrome, Safari and Opera
+                document.documentElement.webkitRequestFullscreen();
+            } else if (document.documentElement.msRequestFullscreen) {
+                // IE/Edge
+                document.documentElement.msRequestFullscreen();
+            }
+        }
+    }
+
+    function exitFullScreen() {
+        if (document.fullscreenElement) {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+                // @ts-ignore
+            } else if (document.mozCancelFullScreen) {
+                // Firefox
+                document.mozCancelFullScreen();
+            } else if (document.webkitExitFullscreen) {
+                // Chrome, Safari and Opera
+                document.webkitExitFullscreen();
+            } else if (document.msExitFullscreen) {
+                // IE/Edge
+                document.msExitFullscreen();
+            }
+        }
+    }
+
+    // Expand button 
+    function toggleExpandVisibility() {
+        clearTimeout(timeout);
+        expandVisible = true;
+
+        timeout = setTimeout(() => {
+            expandVisible = false;
+        }, expandButtonTimeout);
+    }
+
+    onMount(() => {
+        window.addEventListener("mousemove", toggleExpandVisibility);
+        window.addEventListener("keydown", toggleExpandVisibility);
+
+        return () => {
+            window.removeEventListener("mousemove", toggleExpandVisibility);
+            window.removeEventListener("keydown", toggleExpandVisibility);
+        };
+    });
+
+    function handleButtonClick() {
+        enterFullScreen();
+        exitFullScreen();
+    }
 </script>
 
-<div class="container" style="background-color: rgb({`${r},${g},${b}`})" />
+<svelte:window on:keydown={keyEventHandler} />
+
+<div class="container" style="background-color: rgb({`${r},${g},${b}`})">
+    {#if expandVisible}
+        <button class="expand" on:click={handleButtonClick}>&#x26F6 </button>
+    {/if}
+</div>
 
 <style>
+    .expand {
+        font-size: 60px;
+        background-color: inherit;
+        border: none;
+        color: white;
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        margin-right: 20px;
+        margin-bottom: 20px;
+    }
+
+    .expand:focus {
+      outline: none;
+      border: none;
+    }
+
     .container {
         position: fixed;
         top: 0;
