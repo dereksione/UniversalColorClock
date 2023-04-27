518323e1e9069df7460c55f7a2e38cca2ea4a3bc
0x6861796169 <sayak@medici.haus>
2023-04-27 12:52:18 -0400

    responsive video bar

--- src/routes/VideoWithOverlay.svelte
+++ src/routes/VideoWithOverlay.svelte
@@ -51,7 +51,7 @@
     <div class="overlay-container">
         <video class="responsive-video" src={timelapse} autoplay loop muted />
         <div class="text-overlay vertically-centered">
-            <div class="UCC">UNIVERSAL COLOR CLOCK</div>
+            <div class="UCC">UNIVERSAL <br /> COLOR CLOCK</div>
             <div class="tacky-line">Fluid color. Precision time.</div>
         </div>
         <div class="text-overlay countdown">
@@ -63,29 +63,47 @@
     </div>
 </div>
 
-<style>
+<!--     
+/* .vertically-centered {
+    top: 55%;
+    margin-right: 150px;
+    transform: translateY(-50%);
+    width: 700px;
+    bottom: 30px;
+    right: 30px;
+} */ -->
+
+<!-- <style> 
     .parent-container {
         position: relative;
         display: flex;
         background-color: black;
     }
 
+    .overlay-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        position: relative;
+        color: white;
+    }
+
     .text-overlay {
-        position: absolute;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 1rem;
+    }
+
+    /* .text-overlay {
+        position: relative;
         color: white;
         font-size: 24px;
         font-weight: bold;
         text-shadow: 2px 2px 4px rgba(0, 0, 0, 0);
-    }
-
-    .vertically-centered {
-        top: 55%;
-        margin-right: 150px;
-        transform: translateY(-50%);
-        width: 700px;
-        bottom: 30px;
-        right: 30px;
-    }
+        align-items: center;
+    } */
 
     .responsive-video {
         width: auto;
@@ -107,13 +125,8 @@
     .countdown {
         font-family: SimplexUCCA;
         font-weight: 300;
-        margin-left: 300px;
-        margin-bottom: 80px;
-        bottom: 30px;
-        left: 30px;
         color: rgba(252, 252, 252, 0.5);
     }
-
     .countdown-text {
         font-family: SeravekBasicLight;
         font-size: 20px;
@@ -134,5 +147,80 @@
         font-family: SimplexUCCA;
         font-size: 100px;
         font-weight: 300;
+    }
+</style> -->
+
+<style>
+    .parent-container {
+        position: relative;
+        overflow: hidden;
+        background-color: black;
+    }
+
+    .overlay-container {
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        height: 100%;
+        width: 100%;
+    }
+
+    .text-overlay {
+        position: absolute;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: left;
+        padding: 1rem;
+        color: white;
+        font-size: 24px;
+        font-weight: bold;
+        text-shadow: 2px 2px 4px rgba(0, 0, 0, 0);
+    }
+
+    .vertically-centered {
+        top: 55%;
+        margin-left: 450px;
+        /* padding-left: 23%; */
+        transform: translateY(-50%);
+    }
+
+    .countdown {
+        font-family: SimplexUCCA;
+        text-align: left;
+        font-weight: 300;
+        bottom: 30px;
+        margin-right: 800px;
+        color: rgba(252, 252, 252, 0.5);
+    }
+
+    .countdown-text {
+        text-align: left;
+        width: 100%;
+        margin-bottom: 10px;
+        font-family: SeravekBasicLight;
+        font-size: 20px;
+    }
+
+    .countdown-numbers {
+        font-size: 36px;
+    }
+
+    .UCC {
+        font-family: SimplexUCCA;
+        font-size: 90px;
+        font-weight: 300;
+    }
+
+    .tacky-line {
+        width: 100%;
+        font-family: SeravekBasicLight;
+        padding: 0;
+        margin: 0;
+        font-size: 42px;
+        font-weight: 300;
+        color: black;
     }
 </style>
